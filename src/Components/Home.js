0647bdf7681e4b393bdf192bf04792a8b4d7b793
Tahmina <tahminahagverdiyevaa@gmail.com>
2025-05-20 16:35:05 +0200

    initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Logo from "./Logo"
+
+function Home() {
+    return (
+        <Logo />
+    );
+}
+
+export default Home
---
+++ src/Components/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+    return (
+        <header>
+            <h1 className="text-yellow-200 text-5xl font-serif">RICHT</h1>
+        </header>
+    )
+}
+
+export default Logo
