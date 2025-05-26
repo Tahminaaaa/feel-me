a97aceb4bd8503a93efc3f15e928ae5d25f80870
Tahmina <tahminahagverdiyevaa@gmail.com>
2025-05-26 17:01:37 +0200

    changes in home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "./Logo"
 import humanfeelings from "../Photos/humanfeelings.jpeg"
 
 function Home() {
+
+    const [emotion, setEmotion] = useState("");
+
+    const emotions = ["exhausted", "happy", "calm", "tired"];
+
+
+    useEffect(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+            setEmotion(emotions[index]);
+            index = (index + 1) % emotions.length;
+        }, 1000);
+        return () => clearInterval(interval);
+    },);
+
+
     return (
         <div className="header bg-stone-200 min-h-screen">
             <div className="flex justify-between items-center px-6 py-4">
@@ -10,13 +26,12 @@ function Home() {
                 <div className="menu flex justify-end gap-4 pr-4 font-serif">
                     <p>Why we?</p>
                     <p>Read and write</p>
-                    <button>Get started</button>
                 </div>
             </div>
             <div className="flex flex-col md:flex-row items-center md:items-start px-4 md:px-10">
                 <div className="headword font-serif text-3xl md:text-6xl md:pt-40 md:w-1/2">
                     <p>I feel myself</p>
-                    <span>exhausted</span>
+                    <span className="highlight highlight-indigo-600 highlight-variant-13">{emotion}</span>
                     <div className="subhead font-serif text-2xl pt-10">
                         <p>Read, Write, Relieve</p>
                     </div>
